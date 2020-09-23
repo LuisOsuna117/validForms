b9d0358f85c5257a4e94bc1e159eee975d737be7
Luis717 <hack.jlom@gmail.com>
2020-09-23 16:14:04 -0600

    Adding field listeners

--- validForms.js
+++ validForms.js
@@ -1,3 +1,49 @@
 (function (window, document, undefined) {
+
+    /*
+     * Define the regular expressions that will be used
+     */
+
+    var ruleRegex = /^(.+?)\[(.+)\]$/,
+        numericRegex = /^[0-9]+$/,
+        integerRegex = /^\-?[0-9]+$/,
+        decimalRegex = /^\-?[0-9]*\.?[0-9]+$/,
+        emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        alphaRegex = /^[a-z]+$/i,
+        alphaNumericRegex = /^[a-z0-9]+$/i,
+        alphaDashRegex = /^[a-z0-9_\-]+$/i,
+        naturalRegex = /^[0-9]+$/i,
+        naturalNoZeroRegex = /^[1-9][0-9]*$/i,
+        ipRegex = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i,
+        base64Regex = /[^a-zA-Z0-9\/\+=]/i,
+        numericDashRegex = /^[\d\-\s]+$/,
+        urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+        dateRegex = /\d{4}-\d{1,2}-\d{1,2}/;
     
-})
+    /*
+    * The exposed public object to validate a form:
+    *
+    * @param formNameOrNode - String - The name attribute of the form (i.e. <form name="myForm"></form>)
+    * @param fields - Array - [{
+    *     fieldId: The name of the element (i.e. <input name="myField" />)
+    *     display: 'Field Name'
+    *     rules: required|matches[password_confirm]
+    * }]
+    */
+    
+    var FormValidator = function (formName, fields) {
+        this.form = document.forms[formName];
+        console.log(form);
+        for (var i = 0; i < fields.length; i++){
+            document.getElementById(fields[i].fieldId).addEventListener()
+        }
+    };
+    window.FormValidator = FormValidator;
+})(window, document);
+
+/*
+ * Export as a CommonJS module
+ */
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = FormValidator;
+}
