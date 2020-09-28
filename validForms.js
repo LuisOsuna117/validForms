b1881f86c75ee8f14808d4ebcc67802d4f4bd6e6
LuisOsuna117 <hack.jlom@gmail.com>
2020-09-28 16:34:26 -0600

    Update v0.1.2

--- validForms.js
+++ validForms.js
@@ -8,93 +8,78 @@
     *     fieldId: 'Field ID'
     *     fieldName: 'Field Name'
     *     filterName: 'alpha'
-    *     rules: 'required|matches[password_confirm]'
+    *     rules: 'required|preventCapture'
+    *     onSubmit: myFunction() - Optional
     * }]
     */
 
-    var FormValidator = function (formName, fields) {
+    var FormValidator = function (formName, fields, callback) {
         this.form = document.forms[formName];
         this.fields = fields;
+        this.form.onsubmit = callback;
         for (var i = 0; i < fields.length; i++) {
-            document.getElementById(fields[i].fieldId).addEventListener('keypress', _checkInput, false);
-        }
-
-        function _checkInput(e) {
-            console.log(e);
-            var fieldId = e.target.id;
-            var field = document.getElementById(fieldId);
-            for (var i = 0; i < fields.length; i++) {
-                if (fields[i].fieldId == fieldId) {
-                    var rules = fields[i].rules.split("|");
-                    var filter = fields[i].filterName;
-                    for (var i = 0; i < rules.length; i++){
-                        switch (rules) {
-                            case 'required':
-                                field.setAttribute("required", "");
+            var field = document.getElementById(fields[i].fieldId);
+            field.onpaste = e => e.preventDefault();
+            var rules = fields[i].rules.split("|");
+            for (var j = 0; j < rules.length; j++){
+                switch (rules[j]) {
+                    case 'required':
+                        field.setAttribute("required", "");
+                        break;
+                    case 'preventCapture':
+                        switch (fields[i].filterName) {
+                            case 'alpha':
+                                document.getElementById(fields[i].fieldId).addEventListener('keypress', function(e) {
+                                    _checkInput(e,'alpha');
+                                }, false);
                                 break;
-                            case 'preventCapture':
-                                switch (filter) {
-                                    case 'alpha':
-                                        if (e.charCode != 32) {
-                                            if ((e.charCode < 97 || e.charCode > 122) &&
-                                                (e.charCode < 65 || e.charCode > 90)) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                        break;
-                                    case 'alphaNumeric':
-                                        if (e.charCode != 32) {
-                                            if ((e.charCode < 97 || e.charCode > 122) &&
-                                                (e.charCode < 65 || e.charCode > 90) &&
-                                                (e.charCode < 48 || e.charCode > 57)) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                        break;
-                                    case 'numeric':
-                                        if (e.charCode != 32) {
-                                            if (e.charCode < 48 || e.charCode > 57) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                    default:
-                                        break;
-                                }
+                            case 'alphaNumeric':
+                                document.getElementById(fields[i].fieldId).addEventListener('keypress', function(e) {
+                                    _checkInput(e,'alphaNumeric');
+                                }, false);
                                 break;
-                            case 'visualFeedback':
-                                switch (filter) {
-                                    case 'alpha':
-                                        if (e.charCode != 32) {
-                                            if ((e.charCode < 97 || e.charCode > 122) &&
-                                                (e.charCode < 65 || e.charCode > 90)) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                        break;
-                                    case 'alphaNumeric':
-                                        if (e.charCode != 32) {
-                                            if ((e.charCode < 97 || e.charCode > 122) &&
-                                                (e.charCode < 65 || e.charCode > 90) &&
-                                                (e.charCode < 48 || e.charCode > 57)) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                        break;
-                                    case 'numeric':
-                                        if (e.charCode != 32) {
-                                            if (e.charCode < 48 || e.charCode > 57) {
-                                                e.preventDefault();
-                                            }
-                                        }
-                                    default:
-                                        break;
-                                }
+                            case 'numeric':
+                                document.getElementById(fields[i].fieldId).addEventListener('keypress', function(e) {
+                                    _checkInput(e,'numeric');
+                                }, false);
                                 break;
                             default:
                                 break;
                         }
-                    }
+                        
+                        break;
+                    default:
+                        break;
                 }
+            }
+        }
+        function _checkInput(e,filter) {
+            switch (filter) {
+                case 'alpha':
+                    if (e.charCode != 32) {
+                        if ((e.charCode < 97 || e.charCode > 122) &&
+                            (e.charCode < 65 || e.charCode > 90)) {
+                            e.preventDefault();
+                        }
+                    }
+                    break;
+                case 'alphaNumeric':
+                    if (e.charCode != 32) {
+                        if ((e.charCode < 97 || e.charCode > 122) &&
+                            (e.charCode < 65 || e.charCode > 90) &&
+                            (e.charCode < 48 || e.charCode > 57)) {
+                            e.preventDefault();
+                        }
+                    }
+                    break;
+                case 'numeric':
+                    if (e.charCode != 32) {
+                        if (e.charCode < 48 || e.charCode > 57) {
+                            e.preventDefault();
+                        }
+                    }
+                default:
+                    break;
             }
         };
     };
