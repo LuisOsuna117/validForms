3723f1d1d1aea5189eb8a0b482a599784617e082
LuisOsuna117 <hack.jlom@gmail.com>
2020-09-30 00:06:04 -0600

    update v0.2.0

--- validForms.js
+++ validForms.js
@@ -1,17 +1,16 @@
 (function (window, document, undefined) {
-
     /*
-    * The exposed public object to validate a form:
-    *
-    * @param formName - String - The name attribute of the form (i.e. <form name="myForm"></form>)
-    * @param fields - Array - [{
-    *     fieldId: 'Field ID'
-    *     fieldName: 'Field Name'
-    *     filterName: 'alpha'
-    *     rules: 'required|preventCapture'
-    *     onSubmit: myFunction() - Optional
-    * }]
-    */
+     * The exposed public object to validate a form:
+     *
+     * @param formName - String - The name attribute of the form (i.e. <form name="myForm"></form>)
+     * @param fields - Array - [{
+     *     fieldId: 'Field ID'
+     *     fieldName: 'Field Name'
+     *     filterName: 'alpha'
+     *     rules: 'required|preventCapture'
+     *     onSubmit: myFunction() - Optional
+     * }]
+     */
 
     var FormValidator = function (formName, fields, callback) {
         this.form = document.forms[formName];
@@ -19,74 +18,84 @@
         this.form.onsubmit = callback;
         for (var i = 0; i < fields.length; i++) {
             var field = document.getElementById(fields[i].fieldId);
-            field.onpaste = e => e.preventDefault();
+            field.onpaste = (e) => e.preventDefault();
             var rules = fields[i].rules.split("|");
-            for (var j = 0; j < rules.length; j++){
+            for (var j = 0; j < rules.length; j++) {
                 switch (rules[j]) {
-                    case 'required':
+                    case "required":
                         field.setAttribute("required", "");
                         break;
-                    case 'preventCapture':
+                    case "preventCapture":
                         switch (fields[i].filterName) {
-                            case 'alpha':
-                                document.getElementById(fields[i].fieldId).addEventListener('keydown', function(e) {
-                                    _checkInput(e,'alpha');
-                                }, false);
+                            case "alpha":
+                                document
+                                    .getElementById(fields[i].fieldId)
+                                    .addEventListener(
+                                        "input",
+                                        function (e) {
+                                            _checkInput(e, "alpha");
+                                        },
+                                        false
+                                    );
                                 break;
-                            case 'alphaNumeric':
-                                document.getElementById(fields[i].fieldId).addEventListener('keydown', function(e) {
-                                    _checkInput(e,'alphaNumeric');
-                                }, false);
+                            case "alphaNumeric":
+                                document
+                                    .getElementById(fields[i].fieldId)
+                                    .addEventListener(
+                                        "input",
+                                        function (e) {
+                                            _checkInput(e, "alphaNumeric");
+                                        },
+                                        false
+                                    );
                                 break;
-                            case 'numeric':
-                                document.getElementById(fields[i].fieldId).addEventListener('keydown', function(e) {
-                                    _checkInput(e,'numeric');
-                                }, false);
+                            case "numeric":
+                                document
+                                    .getElementById(fields[i].fieldId)
+                                    .addEventListener(
+                                        "input",
+                                        function (e) {
+                                            _checkInput(e, "numeric");
+                                        },
+                                        false
+                                    );
                                 break;
                             default:
                                 break;
                         }
-                        
+
                         break;
                     default:
                         break;
                 }
             }
         }
-        function _checkInput(e,filter) {
-            console.log(e);
+        function _checkInput(e, filter) {
+            console.log(e.data);
+            var field = document.getElementById(e.target.id);
+            var alpha = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+            var alphaNumeric = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
+            var numeric = "1234567890";
             switch (filter) {
-                case 'alpha':
-                    if (e.keyCode != 32 && e.keyCode != 8) {
-                        if ((e.keyCode < 97 || e.keyCode > 122) &&
-                            (e.keyCode < 65 || e.keyCode > 90)) {
-                            e.preventDefault();
-                        }
+                case "alpha":
+                    if (!alpha.includes(e.data) && e.data != null) {
+                        field.value = field.value.slice(0, -1);
                     }
                     break;
-                case 'alphaNumeric':
-                    if (e.keyCode != 32 && e.keyCode != 8) {
-                        if ((e.keyCode < 97 || e.keyCode > 122) &&
-                            (e.keyCode < 65 || e.keyCode > 90) &&
-                            (e.keyCode < 48 || e.keyCode > 57)) {
-                            e.preventDefault();
-                        }
+                case "alphaNumeric":
+                    if (!alphaNumeric.includes(e.data) && e.data != null) {
+                        field.value = field.value.slice(0, -1);
                     }
                     break;
-                case 'numeric':
-                    if (e.keyCode != 32 && e.keyCode != 8) {
-                        if (e.keyCode < 48 || e.keyCode > 57) {
-                            e.preventDefault();
-                        }
+                case "numeric":
+                    if (!numeric.includes(e.data) && e.data != null) {
+                        field.value = field.value.slice(0, -1);
                     }
                 default:
                     break;
             }
-        };
+        }
     };
-
-
-
 
     window.FormValidator = FormValidator;
 })(window, document);
@@ -94,6 +103,6 @@
 /*
  * Export as a CommonJS module
  */
-if (typeof module !== 'undefined' && module.exports) {
+if (typeof module !== "undefined" && module.exports) {
     module.exports = FormValidator;
 }
